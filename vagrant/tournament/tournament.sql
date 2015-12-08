365eee3a87f4a5c230ad83372e33f3d14629f16f
Diana Fisher <diana.fisher@gmail.com>
2015-12-07 17:45:33 -0800

    Prevent rematches between players

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -12,18 +12,24 @@ DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 -- Connect to the tournament database
 \c tournament
+
 -- Create the players table.
 CREATE TABLE players (
-	name TEXT, 
-	id SERIAL PRIMARY KEY
+	name TEXT,  -- the full player name
+	id SERIAL PRIMARY KEY  -- give each player a unique id.
 );
 
 -- Create the matches table. Each match references two players from the players table - the winner of the match and the loser of the match.
 CREATE TABLE matches (	
 	winner_id INTEGER REFERENCES players (id),  -- winner_id has players.id as a foreign key.
-	loser_id INTEGER REFERENCES players (id),   -- loser_id has players.id as a foreign key.
-	id SERIAL PRIMARY KEY
+	loser_id INTEGER REFERENCES players (id),   -- loser_id has players.id as a foreign key.	
+	CHECK (winner_id <> loser_id), -- prevent having a player matched up with themselves.
+	id SERIAL PRIMARY KEY  -- give each match a unique id.
 );
+
+-- Create a unique index to prevent rematches between players.
+CREATE UNIQUE INDEX matches_unique_idx ON matches
+	(greatest(winner_id, loser_id), least(winner_id, loser_id));
 
 -- Create a view to hold the player standings.
 CREATE VIEW player_standings_view AS select id, name, (SELECT count(*) FROM matches WHERE players.id = matches.winner_id) AS wins, (SELECT count(*) FROM matches WHERE players.id = matches.winner_id OR players.id = matches.loser_id) AS matches FROM players ORDER BY wins DESC;
